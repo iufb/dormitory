29b0714ed8bd17171df11ea3a5b868ba72d380aa
iufb <misakilover228@gmail.com>
2024-05-17 16:10:29 +0500

    feat: select

---
+++ src/shared/ui/Select/Select.tsx
@@ -0,0 +1,45 @@
+import clsx from "clsx";
+import styles from "./Select.module.css";
+import { useState } from "react";
+import { GoTriangleDown, GoTriangleUp } from "react-icons/go";
+interface SelectProps<T> {
+  label: string;
+  items: T[];
+  onSelect: (item: T) => void;
+  getValueString: (item: T) => string;
+}
+export function Select<T>({
+  label,
+  onSelect,
+  getValueString,
+  items,
+}: SelectProps<T>) {
+  const [opened, setOpened] = useState(false);
+  const [selected, setSelected] = useState<T | null>(null);
+  return (
+    <div className={styles.container}>
+      <div className={styles.wrapper} onClick={() => setOpened(!opened)}>
+        <span className={styles.label}>
+          {selected ? getValueString(selected) : label}
+        </span>
+        {opened ? <GoTriangleUp /> : <GoTriangleDown />}
+      </div>
+      {opened && (
+        <ul className={clsx(styles.menu, opened && styles.visible)}>
+          {items.map((item, idx) => (
+            <li
+              onClick={() => {
+                setOpened(false);
+                setSelected(item);
+                onSelect(item);
+              }}
+              key={idx}
+            >
+              {getValueString(item)}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+}
