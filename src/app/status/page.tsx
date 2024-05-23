23e29850c3e0169612f25bad012b5e49b481f827
iufb <misakilover228@gmail.com>
2024-05-23 16:10:17 +0500

    feat: meta data

--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -1,6 +1,9 @@
 import { CheckApplicationForm } from "@/features";
 import { BgWrapper, PageTransition } from "@/shared/ui";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Status",
+};
 export default function Status() {
   return (
     <PageTransition>
--- src/app/submitting/page.tsx
+++ src/app/submitting/page.tsx
@@ -1,6 +1,9 @@
 import { CreateApplicationForm } from "@/features";
 import { BgWrapper, PageTransition } from "@/shared/ui";
-
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Application",
+};
 export default function Submitting() {
   return (
     <PageTransition>
