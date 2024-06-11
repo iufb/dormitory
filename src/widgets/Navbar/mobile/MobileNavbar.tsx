484e08905d97e2c1df3398dbd3683b5fcb134beb
iufb <misakilover228@gmail.com>
2024-06-11 15:46:13 +0500

    fix: mobile styles

--- src/widgets/Navbar/mobile/MobileNavbar.tsx
+++ src/widgets/Navbar/mobile/MobileNavbar.tsx
@@ -2,17 +2,24 @@ import Link from "next/link";
 import styles from "./MobileNavbar.module.css";
 import Image from "next/image";
 import { Menu } from "../menu/Menu";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import clsx from "clsx";
 import { IoClose } from "react-icons/io5";
 import { RxHamburgerMenu } from "react-icons/rx";
 import { headers } from "next/headers";
+import { usePathname } from "next/navigation";
 
 export const MobileNavbar = () => {
   const [expanded, setExpanded] = useState(false);
+  const path = usePathname();
   const mouseActionDelay = (func: () => void) => {
     setTimeout(func, 400);
   };
+  useEffect(() => {
+    if (expanded) {
+      setExpanded(false);
+    }
+  }, [path]);
   return (
     <nav
       className={clsx(styles.nav, expanded ? styles.expanded : styles.normal)}
